57dd615d49758d8881c3a7b9350cfd3115293600
Joshua Tate <jmtate@icloud.com>
2019-01-10 19:55:18 -0800

    Finally got it running on emulator! Finished Challenge

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -36,18 +37,22 @@ public class QuizActivity extends AppCompatActivity {
                 // Note that for the context argument you can't simply use 'this', since
                 //  you are inside of the anonymous inner class, not QuizActivity.
                 // After creating the Toast, call show to get it on the screen.
-                Toast.makeText(QuizActivity.this,
+                Toast toast = Toast.makeText(QuizActivity.this,
                         R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 200);
+                toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+                Toast toast = Toast.makeText(QuizActivity.this,
                         R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,200);
+                toast.show();
             }
         });
     }
