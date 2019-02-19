90c7279f57cafcf7f4495847a52826f48edbf949
Joshua Tate <jmtate@icloud.com>
2019-02-18 21:19:13 -0700

    Finished Chapter 6: Added demonstration of wrapping higher API code in conditional statement

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -34,6 +38,33 @@ public class CheatActivity extends AppCompatActivity {
                 else
                     mAnswerTextView.setText(R.string.false_button);
                 setAnswerShownResult(true);
+
+                // ViewAnimationUtils is only available in API 21 or greater
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+
+                    // createCircularReveal creates an animator from a few parameters:
+                    //  1. The View that will be hidden or shown
+                    //  2 & 3. Set a center position (cx, cy)
+                    //  4. Start radius
+                    //  5. End radius
+                    // This particular animation hides the mShowAnswerButton, it's start
+                    //  radius is the width of the button, and its end radius is 0, so its
+                    //  radius moves from the width of the button to 0.
+                    Animator anim = ViewAnimationUtils.createCircularReveal(
+                            mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
             }
         });
     }
